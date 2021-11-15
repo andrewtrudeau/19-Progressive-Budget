d8216bde3ff90388abfc5373269fe1afd05dd00b
TRUDE135 <72237941+TRUDE135@users.noreply.github.com>
2021-11-15 10:13:15 -0600

    deploy

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+  process.env.MONGODB_URI || 'mongodb://localhost/pwa',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
