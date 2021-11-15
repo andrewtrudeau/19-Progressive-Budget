04b2ca65a0ad2d216ef8a71319c3b518c58dc10e
TRUDE135 <72237941+TRUDE135@users.noreply.github.com>
2021-11-15 10:27:14 -0600

    please deploy

--- server.js
+++ server.js
@@ -15,15 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/pwa',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-  }
-);
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {});
 
 // routes
 app.use(require("./routes/api.js"));
